dc527e75de08164c73796214cccf239d125c9597
Oleg Ivanovskij <oleg.ivanovskij.jazzteam@gmail.com>
2018-07-14 13:25:33 +0300

    added black background plugin

---
+++ plugins/BlackBackgroundPlugin/src/main/java/org/ivanovskiy/plugin/BlackBackgroundPluginImpl.java
@@ -0,0 +1,22 @@
+package org.ivanovskiy.plugin;
+
+import org.ivanovskiy.api.AbstractBackgroundPlugin;
+import org.ivanovskiy.api.BackgroundContext;
+
+import java.awt.*;
+
+/**
+ * Created by Ivanovskij Oleg on 14.07.2018
+ */
+public class BlackBackgroundPluginImpl implements AbstractBackgroundPlugin{
+
+    private BackgroundContext bgContext;
+
+    public void changeBackground() {
+        bgContext.getFrame().setBackground(Color.BLACK);
+    }
+
+    public void init(BackgroundContext bgContext) {
+        this.bgContext = bgContext;
+    }
+}
